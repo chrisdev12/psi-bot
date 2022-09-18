82be3f69641e5061ca2643a3a35fea432931f3b1
Christian Bravo <christian.bravo@masglobalconsulting.com>
2022-09-18 14:18:19 -0500

    basic setup

---
+++ src/reactive/child.jsx
@@ -0,0 +1,5 @@
+const childReact = () => {
+  return <h2>Hello child</h2>;
+};
+
+export default childReact;
---
+++ src/reactive/parent.jsx
@@ -0,0 +1,12 @@
+import ChildReactive from './/child';
+
+const parentReactive = () => {
+  return (
+    <>
+      <h1>Hello parent</h1>
+      <ChildReactive />
+    </>
+  );
+};
+
+export default parentReactive;
